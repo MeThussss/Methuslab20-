9eb104b6542ebc7a09a035d316cc0f50004490d5
MeThussss <mathatt16@gmail.com>
2023-02-21 16:49:15 +0700

    Complete

--- lab20_1.cpp
+++ lab20_1.cpp
@@ -5,8 +5,17 @@ struct Rect{
 	double x,y,w,h;
 };
 
-double overlap(){
-
-
-
+double overlap(Rect a,Rect b){
+	double width = (min((a.x)+a.w,(b.x)+b.w) - max(a.x,b.x));
+	double hight = (min(a.y,b.y) - max((a.y)-a.h,(b.y)-b.h));
+	if(width*hight > 0){
+		return width*hight; 
+	}else{
+		return 0; 
+	}
+}
+int main(){
+	Rect R1 = {1,1,5,5};
+	Rect R2 = {2,2,5,5};	
+	cout << overlap(R1,R2);
 }
